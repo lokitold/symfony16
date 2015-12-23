d065f0fccb68e7c9a870734e04d5b8cdcd521d66
victor Rojas <victor.rojas@ec.pe>
2015-12-23 17:05:57 -0500

    cambiio

--- web/test.php
+++ web/test.php
@@ -6,4 +6,34 @@
  * Time: 04:52 PM
  */
 
-echo "aca";Exit;
+require __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+
+$request = Request::createFromGlobals();
+
+// the URI being requested (e.g. /about) minus any query parameters
+$request->getPathInfo();
+
+// retrieve GET and POST variables respectively
+$request->query->get('foo');
+$request->request->get('bar', 'default value if bar does not exist');
+
+// retrieve SERVER variables
+$request->server->get('HTTP_HOST');
+
+// retrieves an instance of UploadedFile identified by foo
+$request->files->get('foo');
+
+// retrieve a COOKIE value
+$request->cookies->get('PHPSESSID');
+
+// retrieve an HTTP request header, with normalized, lowercase keys
+$request->headers->get('host');
+$request->headers->get('content_type');
+
+$request->getMethod();    // GET, POST, PUT, DELETE, HEAD
+$languages = $request->getLanguages(); // an array of languages the client accepts
+echo "<pre>";
+print_r($languages);
+echo "<pre>";
