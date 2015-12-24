b8f1f114082de790d10ce49e27cb4f7e2f5075ff
victor Rojas <victor.rojas@ec.pe>
2015-12-24 12:53:06 -0500

    cambio

--- web/test.php
+++ web/test.php
@@ -9,31 +9,16 @@
 require __DIR__.'/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 $request = Request::createFromGlobals();
 
-// the URI being requested (e.g. /about) minus any query parameters
-$request->getPathInfo();
+$response = new Response();
 
-// retrieve GET and POST variables respectively
-$request->query->get('foo');
-$request->request->get('bar', 'default value if bar does not exist');
+$response->setContent('<html><body><h1>Hello world!</h1></body></html>');
+$response->setStatusCode(Response::HTTP_OK);
+$response->headers->set('Content-Type', 'text/html');
 
-// retrieve SERVER variables
-$request->server->get('HTTP_HOST');
-
-// retrieves an instance of UploadedFile identified by foo
-$request->files->get('foo');
-
-// retrieve a COOKIE value
-$request->cookies->get('PHPSESSID');
-
-// retrieve an HTTP request header, with normalized, lowercase keys
-$request->headers->get('host');
-$request->headers->get('content_type');
-
-$request->getMethod();    // GET, POST, PUT, DELETE, HEAD
-$languages = $request->getLanguages(); // an array of languages the client accepts
-echo "<pre>";
-print_r($languages);
-echo "<pre>";
+// prints the HTTP headers followed by the content
+$response->send();
