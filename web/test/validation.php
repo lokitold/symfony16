00b653868ca12e0a7933bc37be3b28519d469fd9
victor Rojas <victor.rojas@ec.pe>
2016-01-15 15:01:16 -0500

    cambiol

--- web/test/validation.php
+++ web/test/validation.php
@@ -2,25 +2,13 @@
 
 require __DIR__.'/../../vendor/autoload.php';
 
-$input = [
-    'emails' => [
-        7 => 'david@panmedia.co.nz',
-        12 => 'some@email.add',
-    ],
-    'user' => 'bob',
-    'amount' => 7,
-];
-
 use Symfony\Component\Validator\Validation;
-use Symfony\Component\Validator\Constraints;
-$validator = Validation::createValidator();
-$constraint = new Constraints\Collection(array(
-    'emails' => new Constraints\All(array(
-        new Constraints\Email(),
-    )),
-    'user' => new Constraints\Regex('/[a-z]/i'),
-    'amount' => new Constraints\Range(['min' => 5, 'max' => 10]),
-));
+use Symfony\Component\Validator\Constraints\Length;
 
-$violations = $validator->validateValue($input, $constraint);
-echo $violations;
+$validator = Validation::createValidator();
+
+$violations = $validator->validate('Bernhadsssrd', new Length(array('min' => 10)));
+
+echo "<pre>";
+var_dump($violations);
+echo "</pre>";
