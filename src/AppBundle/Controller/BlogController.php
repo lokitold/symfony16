943503df75adfe4dd529b792cbcc6c8430bc70c1
victor Rojas <victor.rojas@ec.pe>
2016-01-20 15:56:14 -0500

    cambio

---
+++ src/AppBundle/Controller/BlogController.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: victor
+ * Date: 20/01/16
+ * Time: 03:44 PM
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class BlogController  extends Controller
+{
+    public function listAction()
+    {
+        $posts = $this->get('doctrine')
+            ->getManager()
+            ->createQuery('SELECT p FROM AcmeBlogBundle:Post p')
+            ->execute();
+
+        return $this->render('AcmeBlogBundle:Blog:list.html.php', array('posts' => $posts));
+    }
+
+    /*
+     *
+     * blog_show:
+    path:     /blog/show/{id}
+    defaults: { _controller: AcmeBlogBundle:Blog:show }
+     * */
+
+    /**
+     * @Route("/blog/show/{id}", name="blogshow")
+     */
+
+    public function showAction($id)
+    {
+        return new Response($id);
+        /*$post = $this->get('doctrine')
+            ->getManager()
+            ->getRepository('AcmeBlogBundle:Post')
+            ->find($id);
+
+        if (!$post) {
+            // hace que se muestre la página de error 404
+            throw $this->createNotFoundException();
+        }
+
+        return $this->render('AcmeBlogBundle:Blog:show.html.php', array('post' => $post));*/
+    }
+}
