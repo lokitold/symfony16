b6b8f5f102ae0e9566f828f6b62e92c053eff937
victor Rojas <victor.rojas@ec.pe>
2016-01-25 16:59:27 -0500

    cvambio

---
+++ src/AppBundle/Entity/Product.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: victor
+ * Date: 25/01/16
+ * Time: 04:56 PM
+ */
+
+namespace AppBundle\Entity;
+
+class Product
+{
+    protected $name;
+    protected $price;
+    protected $description;
+}
