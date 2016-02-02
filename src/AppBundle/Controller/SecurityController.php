e79f8e8597084a51b4f1e19b636119e7cbb87d7f
victor Rojas <victor.rojas@ec.pe>
2016-02-02 13:13:49 -0500

    cambio

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -22,8 +22,25 @@ class SecurityController extends Controller
      */
     public function loginAction(Request $request)
     {
-        $data = array();
-        return $this->render('AppBundle:blog:login.html.twig', array('data' => $data));
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render(
+            'security/login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
+
+        //$data = array();
+        //return $this->render('AppBundle:blog:login.html.twig', array('data' => $data));
     }
 
     /**
