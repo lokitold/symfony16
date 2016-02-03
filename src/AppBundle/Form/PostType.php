aa996810b6446b3dcdea1aa4f69bd64d0570dbfd
victor Rojas <victor.rojas@ec.pe>
2016-02-03 12:28:46 -0500

    cambio

--- src/AppBundle/Form/PostType.php
+++ src/AppBundle/Form/PostType.php
@@ -5,6 +5,7 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use AppBundle\Form\CategoryType;
 
 class PostType extends AbstractType
 {
@@ -16,7 +17,7 @@ class PostType extends AbstractType
     {
         $builder
             ->add('description')
-            ->add('category')
+            ->add('category', CategoryType::class)
         ;
     }
     
